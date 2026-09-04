dd02e652bc7a6ea9caa2cdc7a479fd463643a474
Sølvsteen <194155481+solvsteen@users.noreply.github.com>
2026-09-04 10:35:17 +0200

    Add properties for CurrentMana and MaxMana, and a method for CastSpell in ISpellcaster interface; introduce Roll method in IDiceRoller interface

--- Combat/ISpellcaster.cs
+++ Combat/ISpellcaster.cs
@@ -1,6 +1,10 @@
+using Eventyrerlauget.Dice;
+
 namespace Eventyrerlauget.Combat;
 
 public interface ISpellcaster
 {
-    
+    int CurrentMana { get; }
+    int MaxMana { get; }
+    string CastSpell(IDamageable target, IDiceRoller diceRoller);
 }
--- Dice/IDiceRoller.cs
+++ Dice/IDiceRoller.cs
@@ -2,5 +2,5 @@ namespace Eventyrerlauget.Dice;
 
 public interface IDiceRoller
 {
-    
+    int Roll(int sides);
 }
